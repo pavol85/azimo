1d1a8dedc842dfffb8a8eaceb516e01fa2c6e874
gabryel <pawel.gabryel@geomatyka-krakow.pl>
2016-06-24 00:26:37 +0200

    #3 - install Backbone

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 import 'materialize-css/bin/materialize.css'
 import 'babel-polyfill'
 import $ from "jquery"
+import _ from "underscore"
+import Backbone from "backbone"
 import 'materialize-css/bin/materialize.js'
 import mockData from './js/mockdata'
 import RepositoryCollection from './js/repository.collection'
